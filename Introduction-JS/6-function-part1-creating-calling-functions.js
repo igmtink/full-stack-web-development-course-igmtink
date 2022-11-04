8ca8de208af1662ebb0b375963909ae1190e38c9
igmtink <igmtse@gmail.com>
2022-11-04 15:45:40 +0800

    Introduction JS

--- Introduction-JS/6-function-part1-creating-calling-functions.js
+++ Introduction-JS/6-function-part1-creating-calling-functions.js
@@ -19,6 +19,15 @@ function sampleFunctionParameters(money) {
 
 sampleFunctionParameters(5);
 
+// !More Parameters
+function sampleFunctionMoreParameters(startingMoney, costPerBottle) {
+  var totalBottleCanBuy = Math.floor(startingMoney / costPerBottle);
+
+  console.log(totalBottleCanBuy);
+}
+
+sampleFunctionMoreParameters(5, 1.5);
+
 // TODO: Life in Weeks
 // Formula
 // totalLeft = currentAge - futureAge
@@ -54,3 +63,34 @@ function lifeInWeeks(age) {
 lifeInWeeks(prompt("What is your current age?"));
 
 // !Outputs & Return Values
+function sampleFunctionReturn(money) {
+  var priceOfBottle = 1.5;
+  var totalBottleCanBuy = Math.floor(money / priceOfBottle);
+
+  // This will return the value in (sampleFunctionReturn)
+  return totalBottleCanBuy;
+}
+
+var totalBottles = sampleFunctionReturn(5);
+alert(totalBottles);
+
+// !Function inside Function
+function getMilk(startingMoney, costPerBottle) {
+  console.log("Total bottles: " + calcBottles(startingMoney, costPerBottle));
+
+  return calcChange(startingMoney, costPerBottle);
+}
+
+function calcBottles(startingMoney, costPerBottle) {
+  var totalBottles = Math.floor(startingMoney / costPerBottle);
+
+  return totalBottles;
+}
+
+function calcChange(startingMoney, costPerBottle) {
+  var change = startingMoney % costPerBottle;
+
+  return change;
+}
+
+console.log("Hello sir, here is your " + getMilk(5, 1.5) + " change.");
