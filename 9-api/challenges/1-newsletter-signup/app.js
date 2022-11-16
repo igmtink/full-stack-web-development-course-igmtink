4bcbc785911389ae88d5f46b385828d2eedd9ddb
igmtink <igmtse@gmail.com>
2022-11-16 16:25:34 +0800

    Full Stack Web Development Updated

--- 9-api/challenges/1-newsletter-signup/app.js
+++ 9-api/challenges/1-newsletter-signup/app.js
@@ -2,64 +2,66 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+const mailchimp = require("@mailchimp/mailchimp_marketing");
 const https = require("https");
 const bodyParser = require("body-parser");
 const request = require("request");
 
 // !To include static files, like css file
 app.use(express.static("public"));
+// !To get value of (form input) in html
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
   res.sendFile(`${__dirname}/signup.html`);
 });
 
-app.post("/", function (req, res) {
-  const apiKey = "727970609c0da8d609e1c5fbb55b6bf0-us21";
-  const audienceId = "a4e6dda177";
+const apiKey = "727970609c0da8d609e1c5fbb55b6bf0-us21";
+const audienceId = "a4e6dda177";
+const mailchimp_server = "us21";
 
+// !Setting up MailChimp
+mailchimp.setConfig({
+  apiKey: apiKey,
+  server: mailchimp_server,
+});
+
+app.post("/", function (req, res) {
   // !Input name from html
   const firstName = req.body.fName;
   const lastName = req.body.lName;
   const email = req.body.email;
 
-  // !Data requirements to post a request
-  const data = {
-    members: [
-      {
-        email_address: email,
+  // !Creating an object with the users data
+  const subscribingUser = {
+    firstName: firstName,
+    lastName: lastName,
+    email: email,
+  };
+
+  // !Uploading all data in mailchimp
+  async function run() {
+    // Try/Catch method
+    // !Success
+    try {
+      const response = await mailchimp.lists.addListMember(audienceId, {
+        email_address: subscribingUser.email,
         status: "subscribed",
         merge_fields: {
-          FNAME: firstName,
-          LNAME: lastName,
+          FNAME: subscribingUser.firstName,
+          LNAME: subscribingUser.lastName,
         },
-      },
-    ],
-  };
+      });
 
-  // !To turn the data into string that is in format of json
-  const jsonData = JSON.stringify(data);
+      res.sendFile(`${__dirname}/success.html`);
 
-  // !To get a post request on mailchimp, this is provided by documentation
-  const url = `https://us21.api.mailchimp.com/3.0/lists/${audienceId}`;
-  const options = {
-    method: "POST",
-    auth: `igmtdev:${apiKey}`,
-  };
+      // !Catching error
+    } catch (e) {
+      res.sendFile(`${__dirname}/failure.html`);
+    }
+  }
 
-  // !Get the request from url
-  const request = https.request(url, options, function (response) {
-    // !Get the data from post request url and it will return an a hexidecimals
-    response.on("data", function (data) {
-      // !Convert the hexidecimals to json format
-      console.log(`Data: ${JSON.parse(data)}`);
-    });
-  });
-
-  console.log(`Request: ${request}`);
-
-  // !Pass to (jsonData) to (https.request) to send the mailchimp requirements data by calling (request)
-  request.write(jsonData);
-  request.end();
+  run();
 });
 
 app.listen(port, function () {
