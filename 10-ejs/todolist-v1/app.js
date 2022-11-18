e26baca555c44919aa49ac5c9e26ee4b1580a707
igmtink <igmtse@gmail.com>
2022-11-18 23:52:15 +0800

    Full Stack Web Development Updated

--- 10-ejs/todolist-v1/app.js
+++ 10-ejs/todolist-v1/app.js
@@ -2,18 +2,40 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+// !Express use EJS as an View Engine
+app.set("view engine", "ejs");
+
+// !List of items
+let newItems = [];
 
 app.get("/", function (req, res) {
-  // !Date module
+  // !Get the date
   const today = new Date();
 
-  // !(getDay) getting monday to sunday by returning number (0 = sunday & 6 = saturday)
-  if (today.getDay() === 6 || today.getDay() === 0) {
-    res.send(`Yay it's weekend!`);
-  } else {
-    res.send(`It's weekdays.`);
-  }
+  let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  };
+
+  let day = today.toLocaleDateString("en-US", options);
+
+  // !Passing the value to (html)
+  res.render("list", { kindOfDay: day, newListItems: newItems });
+});
+
+app.post("/", function (req, res) {
+  let newItem = req.body.newItem;
+
+  // !Pass the value from newItem (input value) to newItems (array)
+  newItems.push(newItem);
+
+  // !Whenever we click sumbit button it will redirect to ('/') home route and it will trigger all code in ('/') home route
+  res.redirect("/");
 });
 
 app.listen(port, function () {
