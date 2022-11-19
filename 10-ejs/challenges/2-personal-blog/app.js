4ac50c6c6d85ad4861fe533e8b77681935704079
igmtink <igmtse@gmail.com>
2022-11-20 00:36:16 +0800

    Full Stack Web Development Updated

--- 10-ejs/challenges/2-personal-blog/app.js
+++ 10-ejs/challenges/2-personal-blog/app.js
@@ -3,6 +3,8 @@ const app = express();
 const port = 3000;
 
 const bodyParser = require("body-parser");
+// !To control string
+const _ = require("lodash");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -40,6 +42,24 @@ app.post("/compose", function (req, res) {
 
   // !Passing the from (/compose) to (/blog)
   res.redirect("/blog");
+});
+
+app.get("/post/:topic", function (req, res) {
+  // !Using (lodash) we can control our string this is will convert the capitalize into lowercase and also it will ignore the symbol like hypen (-)
+  const requestTitle = _.lowerCase(req.params.topic);
+
+  // !For loop to check if the url title is in post title array
+  status.forEach(function (post) {
+    const storedTitle = _.lowerCase(post.title);
+
+    // !If post title is equal to url title then render new page according to the (title)
+    if (requestTitle === storedTitle) {
+      res.render("post", {
+        postTitle: post.title,
+        postDescription: post.description,
+      });
+    }
+  });
 });
 
 app.listen(port, function () {
