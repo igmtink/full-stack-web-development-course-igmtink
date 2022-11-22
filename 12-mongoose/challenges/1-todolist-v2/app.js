d8d3e0025c9c48079c951b4c96f66f416209f1cc
igmtink <igmtse@gmail.com>
2022-11-22 23:32:49 +0800

    Full Stack Web Development Updated

--- 12-mongoose/challenges/1-todolist-v2/app.js
+++ 12-mongoose/challenges/1-todolist-v2/app.js
@@ -19,6 +19,8 @@ app.set("view engine", "ejs");
 // !To set our default (static) file from (public) folder
 app.use(express.static("public"));
 
+// !DATABASE
+
 // !Create (Schema / Pattern)
 const todolistSchema = new mongoose.Schema({
   name: { type: String, required: true },
@@ -37,9 +39,11 @@ const todoStart3 = new Todolist({ name: "<-- Hit this to delete an item." });
 // !Insert our default items in (array)
 const todoStart = [todoStart1, todoStart2, todoStart3];
 
+// !HOME ROUTE
+
 // !To render our (html) in (browser)
 app.get("/", function (req, res) {
-  // !Read all (Documents / Field and Value)
+  // !Read all (Documents / Field and Value), (find({})) this is returning an array []
   Todolist.find({}, function (err, todoItems) {
     // !Check if our (Model / Collections) doesn't have any (Documents / Field and Value)
     if (todoItems.length === 0) {
@@ -56,22 +60,93 @@ app.get("/", function (req, res) {
       res.redirect("/");
     } else {
       // !If our (Model / COllections) is not empty, then we render our (html) with passing all (Documents / Field and Value) from (Model / Collections) and it return an array
-      res.render("index", { todoList: todoItems, today: date.getDate() });
+      res.render("index", { todoList: todoItems, listTitle: date.getDate() });
     }
   });
 });
+
+// !CUSTOM ROUTE WITH DATABASE
+
+// !Create (Schema / Pattern) for (custom route)
+const listSchema = new mongoose.Schema({
+  // !Name of the (custom route)
+  name: String,
+  // !(Documents / Field and Value) that (Schema / Pattern) of (todolistSchema) in array
+  items: [todolistSchema],
+});
+
+// !Create (Model / Collections)
+const List = new mongoose.model("List", listSchema);
+
+// !To render any (route) using express route parameter (/:custom_parameter)
+app.get("/:custom_listName", function (req, res) {
+  // !Get our (custom route) name
+  const customListName = req.params.custom_listName;
+
+  // !Read single (Documents / Field and Value) in our (Model / Collections) the (custom route) name is already exist, (findOne()) this is returning an (object schema)
+  List.findOne({ name: customListName }, function (err, foundList) {
+    if (!err) {
+      if (!foundList) {
+        // !If (custom route) name is not exist on our (Model / Collections) then create a new (Documents / Field and Value)
+        const list = new List({
+          name: customListName,
+          items: todoStart,
+        });
+
+        list.save();
+
+        // !After adding a new (custom route) we redirect in our (custom route) to refresh the browser
+        res.redirect("/" + customListName);
+      } else {
+        // !If (custom route) is already exist then render that (html) with a new (Documents / Field and Value) from our new (Model / Collections)
+        res.render("index", {
+          listTitle: foundList.name,
+          todoList: foundList.items,
+        });
+      }
+    }
+  });
+});
+
+// !SUBMIT HOME ROUTE FORM
 
 // !To submit our data from (html)
 app.post("/", function (req, res) {
   // !To get our data from (input tag) in our (html) body
   const todo = req.body.todoInput;
 
+  // !To get our data from (button tag) in our (html) body
+  const title = req.body.listTitle;
+
   // !Create a new (Documents / Field and Value) with passing a data from (input tag) in our (html) body
   const todoDocuments = new Todolist({ name: todo });
-  todoDocuments.save();
 
-  // !After creating a new (Documents / Field and Value) we redirect in (app.get) to render our (html) that have a new (Documents / Field and Value)
-  res.redirect("/");
+  if (title === date.getDate()) {
+    // !If the list title is equal to date, we save our (Documents / Field and Value) in our home route (Model / Collections)
+    todoDocuments.save();
+    res.redirect("/");
+  } else {
+    // !Else find that listTitle (Documents / Field and Value) in custom route (Model / Collections), this is returing an a (object schema)
+    List.findOne({ name: title }, function (err, foundList) {
+      // !Add todoDocuments (Documents / Field and Value) in listTitle (Documents / Field Value) which is inside of (foundList object schema)
+      foundList.items.push(todoDocuments);
+      foundList.save();
+      res.redirect("/" + title);
+    });
+  }
+});
+
+// !SUBMIT DELETE ROUTE FORM
+
+// !To submit our delete post method
+app.post("/delete", function (req, res) {
+  // !To get our value (_id) in (input:check) from (html) body
+  const checkedId = req.body.checkedTodo;
+
+  // !To delete a (Documents / Field and Value) by (_id)
+  Todolist.findByIdAndRemove(checkedId, function (err) {
+    res.redirect("/");
+  });
 });
 
 app.listen(port, function () {
